64a12541a7c66d34b1db6135de47ae8f6cb94e4a
Chandra <adityanugraha7251@gmail.com>
2025-07-09 01:31:52 +0800

    CHECKPOIN PENTING

---
+++ resources/views/homePembeli.blade.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="style/homepage.css?v=1.2"/>
+  <link rel="stylesheet" href="style/sidebar.css?v=1.2">
+  <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+  <link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
+
+  <title>Home</title>
+</head>
+
+<body>
+  <div class="background">
+
+        <div class="inner-background">
+
+        
+        
+            <div class="inner-header">
+
+                <div class="weather">
+                    <div class="judul">
+                        <h1>HOME</h1>
+                    </div>
+                    <div class="cuaca">
+                        <div class="forecast">
+                            <ion-icon name="partly-sunny-outline"></ion-icon>
+                        </div>
+                        <div class="suhu">
+                            <h1>23°C</h1>
+                        </div>
+                    </div>
+                <p>The weather is perfect for a ride</p>
+                </div>
+
+                <div class="navigation">
+                    <div class="username">Welcome, user123456</div>
+                    <a href="profilepage.html">
+                      <div class="profile-pic"></div>
+                    </a>
+                   
+                </div>
+            </div>
+            
+            <div class="inner-body">
+
+            <div class="best-deals">
+                <h2>Best Deals</h2>
+               
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+
+                        <div class="deal-texts">
+                          <h3>Wayan Rentals</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          <ion-icon name="chatbubble-outline"></ion-icon>
+                          <ion-icon name="star-outline"></ion-icon>
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+                        
+                        <div class="deal-texts">
+                          <h3>Wayan Rentals</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          <ion-icon name="chatbubble-outline"></ion-icon>
+                          <ion-icon name="star-outline"></ion-icon>
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+                        
+                        <div class="deal-texts">
+                          <h3>Wayan Rentals</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          <ion-icon name="chatbubble-outline"></ion-icon>
+                          <ion-icon name="star-outline"></ion-icon>
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+    
+ 
+
+            </div>
+
+
+            <div class="widgets">
+                <div class="left-column">
+                  <!-- Large element on the left -->
+                  <div class="map-container">
+                    <img class="rentals-nearby" src="gambar/peta.png" alt="">
+                  </div>
+                </div>
+              
+                <div class="right-column">
+                  <!-- Smaller boxes stacked on top of each other -->
+                  <div class="return-date">
+                    <div class="calendar">
+                      <ion-icon name="calendar-clear-outline"></ion-icon>
+                    </div>
+
+                    <div class="tanggal-balik">
+                      <h3>return before</h3>
+                      <h1>31/12/2025</h1>
+                    </div>
+                   
+                  </div>
+                  <div class="promos">
+                    <div class="promo1"></div>
+                    <div class="promo1"></div>
+                  </div>
+                </div>
+              </div>
+
+            <div class="store-rec">
+                <div class="rec-header">
+                  <h3>Store Recommendation</h3>
+                </div>
+                <div class="rec-body"></div>
+            </div>
+            </div>
+        </div>
+
+        <nav class="sidebar">
+          <header>
+            <div class="image-text">
+              <span class="image">
+                <img src="gambar/logo.png" alt="logo"><!-- masukin logo apk kita tar-->
+              </span>
+
+              <div class="text header-text">
+                <span class="name">GASS</span>
+                <span class="profession">Go And Start the Street</span>
+              </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+          </header>
+          
+          <div class="menu-bar">
+            <div class="menu">
+
+              <li class="search-box">
+                  
+                <i class='bx bx-search-alt-2 icon' ></i>
+                <input type="search" placeholder="Search...">
+                  
+              </li>
+              <ul class="menu-links">
+                <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-home-alt icon' ></i>
+                    <span class="text nav-text">Home</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="profilepage.html">
+                    <i class='bx bx-user icon' ></i>
+                    <span class="text nav-text">Profile</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-bell icon' ></i>
+                    <span class="text nav-text">Notif</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="roomchat.html">
+                    <i class='bx bx-conversation icon' ></i>
+                    <span class="text nav-text">Chat</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="tokopage.html">
+                    <i class='bx bx-store-alt icon' ></i>
+                    <span class="text nav-text">Stores</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="tokopage.html">
+                    <i class='bx bx-history icon' ></i>
+                    <span class="text nav-text">History</span>
+                  </a>
+                </li>
+
+              </ul>
+            </div>
+
+            <div class="bottom-konten">
+              <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-cog icon' ></i>
+                    <span class="text nav-text">Settings</span>
+                  </a>
+              </li>
+              <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-log-out icon' ></i>
+                    <span class="text nav-text">Log out</span>
+                  </a>
+              </li>
+            </div>
+
+          </div>
+        </nav>
+  </div>
+
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  <script src="scripts/sidebar.js"></script>
+
+</body>
+</html>
---
+++ resources/views/homePeminjam.blade.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="style/peminjam.css?v=1.2"/>
+  <link rel="stylesheet" href="style/sidebar.css?v=1.2">
+  <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+  <link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
+
+  <title>Home</title>
+</head>
+
+<body>
+    <div class="background">
+
+        <div class="inner-background">
+
+        
+        
+            <div class="inner-header">
+
+                <div class="stat-toko">
+                    <div class="elemen">
+                        <div class="judul">
+                            <h1>HOME</h1>
+                        </div>
+                        <div class="location">
+                            <ion-icon name="location-outline"></ion-icon>
+                            <p>Jl Bukit Lestari B 11</p>
+                        </div>
+                        <div class="viewer">
+                            <h4>Your Store Viewership</h4>
+                            <ion-icon name="eye-outline"></ion-icon>
+                            32
+                        </div>
+                    </div>
+                </div>
+                <div class="user-info">
+                    <div class="navigation">
+                        <div class="username">Welcome, user123456</div>
+                        <a href="profilepage.html">
+                            <div class="profile-pic"></div>
+                        </a>
+                    </div>
+                    <div class="icons">
+                        <div class="notif"><ion-icon name="notifications-outline"></ion-icon></div>
+                        <div class="clock"><ion-icon name="time-outline"></ion-icon></div>
+                        <div class="stat"><ion-icon name="stats-chart-outline"></ion-icon></div>
+                    </div>
+                </div>
+                
+            </div>
+
+        <div class="inner-body">
+
+            <div class="best-deals">
+                <h2>Your Catalog</h2>
+               
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+
+                        <div class="deal-texts">
+                          <h3>Motor 1</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+                        
+                        <div class="deal-texts">
+                          <h3>Motor 2</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="deals1">
+                    <div class="deals-kiri">
+                      <div class="deals-elements">
+                        
+                        <div class="deal-texts">
+                          <h3>Motor 3</h3>
+                          <p>Start from</p>
+                          <div class="price-badge">IDR 750.000</div>
+                        </div>
+                        
+                        <div class="deal-icons">
+                          
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="deals-kanan">
+                        <div class="container-gambar">
+                          <img class="gambar-deals" src="gambar/motorbike.png" alt="">
+                        </div>
+                    </div>
+                </div>
+    
+ 
+
+            </div>
+
+
+            
+            <div class="add-catalog">
+                <div class="add-catalog-header">
+                    <div class="img-container">
+                        <img class="add-pic" src="gambar/addBike.png" alt="">
+                    </div>
+                </div>
+                <div class="add-catalog-body">
+                    <div class="add-button">
+                        <ion-icon name="add-circle-outline"></ion-icon>
+                    </div>
+                </div>
+            </div>
+
+            <div class="store-rec">
+                <div class="rec-header">
+                  <h3>Reviews</h3>
+                </div>
+                <div class="rec-body">
+                    <div class="review1">
+                        <h2>User123</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                           Quod dignissimos nobis totam natus iure sapiente vitae est officiis 
+                           asperiores ab? Fuga, officia quis sed iusto incidunt reiciendis facere? 
+                           Error, quas!
+                        </p>
+                    </div> 
+                    <div class="review1">
+                        <h2>User123</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                           Quod dignissimos nobis totam natus iure sapiente vitae est officiis 
+                           asperiores ab? Fuga, officia quis sed iusto incidunt reiciendis facere? 
+                           Error, quas!
+                        </p>
+                    </div>  
+                    <div class="review1">
+                        <h2>User123</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                           Quod dignissimos nobis totam natus iure sapiente vitae est officiis 
+                           asperiores ab? Fuga, officia quis sed iusto incidunt reiciendis facere? 
+                           Error, quas!
+                        </p>
+                    </div>            
+                </div>
+            </div>
+        </div>
+        </div>
+
+        <nav class="sidebar close">
+          <header>
+            <div class="image-text">
+              <span class="image">
+                <img src="gambar/logo.png" alt="logo"><!-- masukin logo apk kita tar-->
+              </span>
+
+              <div class="text header-text">
+                <span class="name">GASS</span>
+                <span class="profession">Go And Start the Street</span>
+              </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+          </header>
+          
+          <div class="menu-bar">
+            <div class="menu">
+
+              <li class="search-box">
+                  
+                <i class='bx bx-search-alt-2 icon' ></i>
+                <input type="search" placeholder="Search...">
+                  
+              </li>
+              <ul class="menu-links">
+                <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-home-alt icon' ></i>
+                    <span class="text nav-text">Home</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="profilepage.html">
+                    <i class='bx bx-user icon' ></i>
+                    <span class="text nav-text">Profile</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-bell icon' ></i>
+                    <span class="text nav-text">Notif</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="roomchat.html">
+                    <i class='bx bx-conversation icon' ></i>
+                    <span class="text nav-text">Chat</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="tokopage.html">
+                    <i class='bx bx-store-alt icon' ></i>
+                    <span class="text nav-text">My Catalog</span>
+                  </a>
+                </li>
+                <li class="nav-link">
+                  <a href="tokopage.html">
+                    <i class='bx bx-history icon' ></i>
+                    <span class="text nav-text">History</span>
+                  </a>
+                </li>
+
+              </ul>
+            </div>
+
+            <div class="bottom-konten">
+              <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-cog icon' ></i>
+                    <span class="text nav-text">Settings</span>
+                  </a>
+              </li>
+              <li class="nav-link">
+                  <a href="#">
+                    <i class='bx bx-log-out icon' ></i>
+                    <span class="text nav-text">Log out</span>
+                  </a>
+              </li>
+            </div>
+            
+          </div>
+        </nav>
+  </div>
+
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  <script src="scripts/sidebar.js"></script>
+
+</body>
+</html>
